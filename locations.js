734170af285e4da47248293a405910102d254b4a
aaraynaaryan8-sketch <aaraynaaryan8@gmail.com>
2026-09-01 18:02:20 +0530

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,19 @@
+const locations = {
+  "NEXA-001": {
+    name: "Taj Mahal, Agra",
+    lat: 27.1751,
+    lon: 78.0421
+  },
+
+  "NEXA-002": {
+    name: "India Gate, New Delhi",
+    lat: 28.6129,
+    lon: 77.2295
+  },
+
+  "NEXA-003": {
+    name: "Gateway of India, Mumbai",
+    lat: 18.9220,
+    lon: 72.8347
+  }
+};
